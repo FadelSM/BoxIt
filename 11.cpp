8dce782196caa4a30d6f78576a54425cd6b15227
FadelSM <rasyahstore8888@gmail.com>
2026-02-08 13:26:31 +0700

    Implement Box class with constructors and methods

--- 11.cpp
+++ 11.cpp
@@ -1,27 +1,62 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-//Implement the class Box  
-//l,b,h are integers representing the dimensions of the box
+class Box {
+private:
+    int l, b, h;
 
-// The class should have the following functions : 
+public:
+    // Default constructor
+    Box() {
+        l = b = h = 0;
+    }
 
-// Constructors: 
-// Box();
-// Box(int,int,int);
-// Box(Box);
+    // Parameterized constructor
+    Box(int length, int breadth, int height) {
+        l = length;
+        b = breadth;
+        h = height;
+    }
 
+    // Copy constructor
+    Box(const Box& B) {
+        l = B.l;
+        b = B.b;
+        h = B.h;
+    }
 
-// int getLength(); // Return box's length
-// int getBreadth (); // Return box's breadth
-// int getHeight ();  //Return box's height
-// long long CalculateVolume(); // Return the volume of the box
+    // Getters
+    int getLength() {
+        return l;
+    }
 
-//Overload operator < as specified
-//bool operator<(Box& b)
+    int getBreadth() {
+        return b;
+    }
 
-//Overload operator << as specified
-//ostream& operator<<(ostream& out, Box& B)
+    int getHeight() {
+        return h;
+    }
+
+    // Volume
+    long long CalculateVolume() {
+        return (long long) l * b * h;
+    }
+
+    // Operator <
+    bool operator<(const Box& B) {
+        if (l < B.l) return true;
+        if (l == B.l && b < B.b) return true;
+        if (l == B.l && b == B.b && h < B.h) return true;
+        return false;
+    }
+
+    // Operator <<
+    friend ostream& operator<<(ostream& out, const Box& B) {
+        out << B.l << " " << B.b << " " << B.h;
+        return out;
+    }
+};
 
 
 void check2()
